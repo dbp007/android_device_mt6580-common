aa81e8f8f32b6049882fba3d744b94ceadf1d52c
Parhib24 <sirajammunirparthi24@gmail.com>
2018-04-11 19:15:18 +0600

    mt6580-common: init
    Signed-off-by: Parhib24 <sirajammunirparthi24@gmail.com>

---
+++ patches/apply-patches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PATCH=$PWD/mtk-diff
+TOP=../../../.. # device/walton/vsun6580_we_m/patches
+
+function apply() {
+ echo [$1 - $2];
+ (cd $TOP/$2; git apply -v $PATCH/$1);
+ echo;
+}
+
+echo TOP            = $(cd $TOP; pwd);
+echo Patches folder = $(cd $PATCH; pwd);
+echo;
+
+# -                  ---PATCH FILE---                      ---FOLDER---
+apply bionic/0001-Apply-LIBC-version-to-__pthread_gettid.patch bionic
+apply external_wpa_supplicant_8/0001-Ignore-NVRAM-ERROR.patch external/wpa_supplicant_8
